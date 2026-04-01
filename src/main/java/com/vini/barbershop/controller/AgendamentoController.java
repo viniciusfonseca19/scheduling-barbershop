246ac558674f78971b19d0c52fecae14005e1f44
Vinícius F. <fonsecavinicius117@gmail.com>
2026-04-01 20:26:45 -0300

    feat: create AgendamentoController with create and delete endpoints

---
+++ src/main/java/com/vini/barbershop/controller/AgendamentoController.java
@@ -0,0 +1,24 @@
+package com.vini.barbershop.controller;
+
+import com.vini.barbershop.entity.Agendamento;
+import com.vini.barbershop.service.AgendamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/agendamentos")
+@RequiredArgsConstructor
+public class AgendamentoController {
+
+    private final AgendamentoService service;
+
+    @PostMapping
+    public Agendamento criar(@RequestBody Agendamento agendamento) {
+        return service.criarAgendamento(agendamento);
+    }
+
+    @DeleteMapping("/{id}")
+    public void cancelar(@PathVariable Long id) {
+        service.cancelarAgendamento(id);
+    }
+}
