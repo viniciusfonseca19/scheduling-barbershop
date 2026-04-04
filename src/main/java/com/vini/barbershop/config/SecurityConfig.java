d4c490c04aa7416f747e9e213020a83514b77437
Vinícius F. <fonsecavinicius117@gmail.com>
2026-04-04 10:52:16 -0300

    fix: allow access to all /usuarios endpoints by using wildcard matcher

--- src/main/java/com/vini/barbershop/config/SecurityConfig.java
+++ src/main/java/com/vini/barbershop/config/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,7 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
 
                         // libera criação de usuário
-                        .requestMatchers("/usuarios").permitAll()
+                        .requestMatchers("/usuarios/**").permitAll()
 
                         // resto precisa estar autenticado
                         .anyRequest().authenticated()
