4d77c6c008dc77cf54c76c2b6de0a6c679c30049
Vinícius F. <fonsecavinicius117@gmail.com>
2026-04-04 11:07:13 -0300

    feat: implement appointment status and secure cancellation logic

--- src/main/java/com/vini/barbershop/service/AgendamentoService.java
+++ src/main/java/com/vini/barbershop/service/AgendamentoService.java
@@ -4,7 +4,10 @@ import com.vini.barbershop.dto.request.AgendamentoRequestDTO;
 import com.vini.barbershop.dto.response.AgendamentoResponseDTO;
 import com.vini.barbershop.entity.Agendamento;
 import com.vini.barbershop.entity.enums.Role;
+import com.vini.barbershop.entity.enums.StatusAgendamento;
 import com.vini.barbershop.entity.Usuario;
+import com.vini.barbershop.exception.BusinessException;
+import com.vini.barbershop.exception.ResourceNotFoundException;
 import com.vini.barbershop.mapper.AgendamentoMapper;
 import com.vini.barbershop.repository.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
@@ -29,16 +32,17 @@ public class AgendamentoService {
         Agendamento agendamento = mapper.toEntity(dto);
 
         agendamento.setUsuario(usuarioLogado);
+        agendamento.setStatus(StatusAgendamento.AGENDADO);
 
         //Regra: não permitir data no passado
         if (agendamento.getData().isBefore(LocalDate.now())) {
-            throw new RuntimeException("Não é possível agendar para uma data no passado");
+            throw new BusinessException("Não é possível agendar para uma data no passado");
         }
 
         //Regra: horário passado no mesmo dia
         if (agendamento.getData().isEqual(LocalDate.now()) &&
                 agendamento.getHorario().isBefore(LocalTime.now())) {
-            throw new RuntimeException("Não é possível agendar para um horário que já passou");
+            throw new BusinessException("Não é possível agendar para um horário que já passou");
         }
 
         //Regra: não permitir horário duplicado
@@ -48,7 +52,7 @@ public class AgendamentoService {
         );
 
         if (existe) {
-            throw new RuntimeException("Horário já está ocupado");
+            throw new BusinessException("Horário já está ocupado");
         }
 
         Agendamento salvo = repository.save(agendamento);
@@ -75,6 +79,20 @@ public class AgendamentoService {
     }
 
     public void cancelarAgendamento(Long id) {
-        repository.deleteById(id);
+
+        Agendamento agendamento = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Agendamento não encontrado"));
+
+        Usuario usuario = usuarioLogadoService.getUsuarioLogado();
+
+        // CLIENTE só cancela o próprio
+        if (usuario.getRole() != Role.ADMIN &&
+                !agendamento.getUsuario().getId().equals(usuario.getId())) {
+            throw new BusinessException("Você não pode cancelar esse agendamento");
+        }
+
+        agendamento.setStatus(StatusAgendamento.CANCELADO);
+
+        repository.save(agendamento);
     }
 }
