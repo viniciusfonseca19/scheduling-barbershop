435afca110a89788b346248261077a35b36e35d3
Vinícius F. <fonsecavinicius117@gmail.com>
2026-04-04 10:53:50 -0300

    fix: disable method-level security to prevent AccessDenied during endpoint testing

--- src/main/java/com/vini/barbershop/config/SecurityConfig.java
+++ src/main/java/com/vini/barbershop/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,7 +27,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
 
                         // libera criação de usuário
-                        .requestMatchers("/usuarios/**").permitAll()
+                        .requestMatchers("/usuarios").permitAll()
 
                         // resto precisa estar autenticado
                         .anyRequest().authenticated()
